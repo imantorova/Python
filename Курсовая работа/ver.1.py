92cc9f21dbe05e914ab6d06c184a4eab45db1f40
imantorova <a9711529@yandex.ru>
2015-11-23 00:57:06 +0300

    Курсовая работа. Не работает bind с кнопкой.

--- Курсовая работа/ver.1.py
+++ Курсовая работа/ver.1.py
@@ -2,6 +2,9 @@
 
 from tkinter import *
 
+def vyvod_v_doc(event):
+    print ('1111')
+
 root=Tk()
 
 class Shablon_text:
@@ -16,16 +19,16 @@ class familiya(Shablon_text):
 
 class imya(Shablon_text):
     def __init__(self):
-        super().__init__('Имя',0,4,2,20,0,6,2)
+        super().__init__('Имя',0,4,2,18,0,6,2)
 
 class otchestvo(Shablon_text):
     def __init__(self):
-        super().__init__('Отчество',0,8,2,20,0,10,2)
+        super().__init__('Отчество',0,8,2,30,0,10,2)
 
 
 class data_rogdeniya (Shablon_text):
     def __init__(self):
-        super().__init__('дата рождения ',1,0,3,20,1,3,3)
+        super().__init__('дата рождения   ',1,0,3,20,1,3,3)
 
 class mesto_rogdeniya (Shablon_text):
     def __init__(self):
@@ -33,7 +36,7 @@ class mesto_rogdeniya (Shablon_text):
 
 class pass_ser (Shablon_text):
     def __init__(self):
-        super().__init__('паспорт серия  ',3,0,3,20,3,3,3)
+        super().__init__('паспорт серия    ',3,0,3,20,3,3,3)
 
 class pass_nom (Shablon_text):
     def __init__(self):
@@ -41,7 +44,7 @@ class pass_nom (Shablon_text):
 
 class data_vydachy (Shablon_text):
     def __init__(self):
-        super().__init__('дата выдачи     ',4,0,3,20,4,3,3)
+        super().__init__('дата выдачи       ',4,0,3,20,4,3,3)
 
 class kem_vydan (Shablon_text):
     def __init__(self):
@@ -49,11 +52,11 @@ class kem_vydan (Shablon_text):
 
 class adres_reg (Shablon_text):
     def __init__(self):
-        super().__init__('адрес прописки ',5,0,3,72,5,3,9)
+        super().__init__('адрес прописки   ',5,0,3,81,5,3,9)
 
 class N_kv (Shablon_text):
     def __init__(self):
-        super().__init__('№ квартиры      ',6,0,3,13,6,3,1)
+        super().__init__('№ квартиры        ',6,0,3,12,6,3,1)
 
 class kadastr_N(Shablon_text):
     def __init__(self):
@@ -61,20 +64,72 @@ class kadastr_N(Shablon_text):
 
 class etag (Shablon_text):
     def __init__(self):
-        super().__init__('Этаж     ',6,10,1,10,6,11,1)
+        super().__init__('Этаж     ',6,10,1,13,6,11,1)
 
-familiya(),imya(),otchestvo()
-data_rogdeniya(),mesto_rogdeniya()
-pass_ser(),pass_nom()
-data_vydachy(),kem_vydan()
-adres_reg()
-N_kv(),kadastr_N(),etag()
+class o_S (Shablon_text):
+    def __init__(self):
+        super().__init__('Общая площадь ',7,0,3,12,7,3,1)
+
+class o_S_balk (Shablon_text):
+    def __init__(self):
+        super().__init__('  кв.м.;  Общая площадь с балконом',7,4,5,15,7,9,2)
+        Label(root, text='кв.м.', font='Arial 10').grid(row=7,column= 11,columnspan= 1)
+
+class S_kuch (Shablon_text):
+    def __init__(self):
+        super().__init__('Площадь кухни  ',8,0,3,12,8,3,1)
+
+class S_gil (Shablon_text):
+    def __init__(self):
+        super().__init__('  кв.м.;                     Жилая площадь',8,4,5,15,8,9,2)
+        Label(root, text='кв.м.', font='Arial 10').grid(row=8,column= 11,columnspan= 1)
+
+class N_zap_reg (Shablon_text):
+    def __init__(self):
+        super().__init__('Рег. № записи    ',9,0,3,12,9,3,1)
+
+class data_reg (Shablon_text):
+    def __init__(self):
+        super().__init__('                             Дата регистрации',9,4,5,15,9,9,2)
+
+class Sum_dog (Shablon_text):
+    def __init__(self):
+        super().__init__('Сумма договора ',10,0,3,81,10,3,9)
+
+class pol:
+    def __init__(self):
+        Label(root, text='пол                    ', font='Arial 10').grid(row=2,column= 0,columnspan= 3)
+        var=IntVar()
+        var.set(1)
+        self.rad0 = Radiobutton(root,text="Мужской",variable=var,value=0).grid(row=2,column= 3,columnspan= 2)
+        self.rad1 = Radiobutton(root,text="Женский",variable=var,value=1).grid(row=2,column= 5,columnspan= 2)
+
+class But_save:
+    def __init__(self):
+        self.but=Button(root, text = 'СОХРАНИТЬ',font='Arial 10').grid(row=11,column=11,columnspan= 2)
+        #self.but.bind("<Button-1>",print('!!!'))
+
+
+
+class zapusk_formy:
+    familiya(),imya(),otchestvo()
+    data_rogdeniya(),mesto_rogdeniya()
+    pol()
+    pass_ser(),pass_nom()
+    data_vydachy(),kem_vydan()
+    adres_reg()
+    N_kv(),kadastr_N(),etag()
+    o_S(),o_S_balk()
+    S_kuch(),S_gil()
+    N_zap_reg(),data_reg()
+    Sum_dog()
+    But_save()
+
+
+zapusk_formy()
 
 root.mainloop()
 
 '''
-var=IntVar()
-var.set(1)
-rad0 = Radiobutton(root,text="Мужской",variable=var,value=0)
-rad1 = Radiobutton(root,text="Женский",variable=var,value=1)
+
 '''
