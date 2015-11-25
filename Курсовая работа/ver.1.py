ac776fb22b1a5f6a757e2a3f16b10936876a5d16
imantorova <a9711529@yandex.ru>
2015-11-25 23:58:22 +0300

    Курсовая работа

--- Курсовая работа/ver.1.py
+++ Курсовая работа/ver.1.py
@@ -7,98 +7,65 @@ def vyvod_v_doc(event):
 
 root=Tk()
 
-class Shablon_text:
-    def __init__(self,text,r1,c1,cs1,w,r2,c2,cs2):
-
+class Shablon_text_label:
+    def __init__(self,text,r1,c1,cs1):
         Label(root, text=text, font='Arial 10').grid(row=r1,column= c1,columnspan= cs1)
-        Entry(root,width=w,bd=2).grid(row=r2,column=c2,columnspan=cs2)
 
-class familiya(Shablon_text):
+class Shablon_text_input:
+    def __init__(self,w,r2,c2,cs2):
+        e=Entry(root,width=w,bd=2)
+        e.grid(row=r2,column=c2,columnspan=cs2)
+
+class labels(Shablon_text_label):
     def __init__(self):
-        super().__init__('Фамилия',0,0,2,20,0,2,2)
+        l={'name':['Фамилия','Имя','Отчество','дата рождения   ','место рождения','паспорт серия    ','номер','дата выдачи       ',
+                  'кем выдан ','адрес прописки   ','№ квартиры        ','Кадастровый №','Этаж     ','Общая площадь ',
+                  '  кв.м.;  Общая площадь с балконом','Площадь кухни  ','  кв.м.;                     Жилая площадь',
+                  'Рег. № записи    ','                             Дата регистрации','Сумма договора ','пол                    '],
+            'lab_row':[0,0,0,1,1,3,3,4,4,5,6,6,6,7,7,8,8,9,9,10,2],
+            'lab_column':[0,4,8,0,6,0,6,0,6,0,0,5,10,0,4,0,4,0,4,0,0],
+            'lab_columnspan':[2,2,2,3,3,3,3,3,3,3,3,2,1,3,5,3,5,3,5,3,3]
+        }
+        for i in range (21):
+            super().__init__(l['name'][i],l['lab_row'][i],l['lab_column'][i],l['lab_columnspan'][i])
 
-class imya(Shablon_text):
+class inputs(Shablon_text_input):
     def __init__(self):
-        super().__init__('Имя',0,4,2,18,0,6,2)
-
-class otchestvo(Shablon_text):
-    def __init__(self):
-        super().__init__('Отчество',0,8,2,30,0,10,2)
-
-
-class data_rogdeniya (Shablon_text):
-    def __init__(self):
-        super().__init__('дата рождения   ',1,0,3,20,1,3,3)
-
-class mesto_rogdeniya (Shablon_text):
-    def __init__(self):
-        super().__init__('место рождения',1,6,3,30,1,9,3)
-
-class pass_ser (Shablon_text):
-    def __init__(self):
-        super().__init__('паспорт серия    ',3,0,3,20,3,3,3)
-
-class pass_nom (Shablon_text):
-    def __init__(self):
-        super().__init__('номер',3,6,3,30,3,9,3)
-
-class data_vydachy (Shablon_text):
-    def __init__(self):
-        super().__init__('дата выдачи       ',4,0,3,20,4,3,3)
-
-class kem_vydan (Shablon_text):
-    def __init__(self):
-        super().__init__('кем выдан ',4,6,3,30,4,9,3)
-
-class adres_reg (Shablon_text):
-    def __init__(self):
-        super().__init__('адрес прописки   ',5,0,3,81,5,3,9)
-
-class N_kv (Shablon_text):
-    def __init__(self):
-        super().__init__('№ квартиры        ',6,0,3,12,6,3,1)
-
-class kadastr_N(Shablon_text):
-    def __init__(self):
-        super().__init__('Кадастровый №',6,5,2,20,6,7,3)
-
-class etag (Shablon_text):
-    def __init__(self):
-        super().__init__('Этаж     ',6,10,1,13,6,11,1)
-
-class o_S (Shablon_text):
-    def __init__(self):
-        super().__init__('Общая площадь ',7,0,3,12,7,3,1)
-
-class o_S_balk (Shablon_text):
-    def __init__(self):
-        super().__init__('  кв.м.;  Общая площадь с балконом',7,4,5,15,7,9,2)
-        Label(root, text='кв.м.', font='Arial 10').grid(row=7,column= 11,columnspan= 1)
-
-class S_kuch (Shablon_text):
-    def __init__(self):
-        super().__init__('Площадь кухни  ',8,0,3,12,8,3,1)
-
-class S_gil (Shablon_text):
-    def __init__(self):
-        super().__init__('  кв.м.;                     Жилая площадь',8,4,5,15,8,9,2)
-        Label(root, text='кв.м.', font='Arial 10').grid(row=8,column= 11,columnspan= 1)
-
-class N_zap_reg (Shablon_text):
-    def __init__(self):
-        super().__init__('Рег. № записи    ',9,0,3,12,9,3,1)
-
-class data_reg (Shablon_text):
-    def __init__(self):
-        super().__init__('                             Дата регистрации',9,4,5,15,9,9,2)
-
-class Sum_dog (Shablon_text):
-    def __init__(self):
-        super().__init__('Сумма договора ',10,0,3,81,10,3,9)
+        #буквы!
+        i_bukva={'dl_bukva':[20,18,30,30],
+                 'i_bukva_row':[0,0,0,1],
+                 'i_bukva_column':[2,6,10,9],
+                 'i_bukva_columnspan':[2,2,2,3]
+        }
+        for i in range (4):
+            b=super().__init__(i_bukva['dl_bukva'][i],i_bukva['i_bukva_row'][i],
+                             i_bukva['i_bukva_column'][i],i_bukva['i_bukva_columnspan'][i])
+        #цифры!
+        i_cifra={'dl_cifra':[20,30,20,12,13,12,15,12,15,81],
+                 'i_cifra_row':[3,3,4,6,6,7,7,8,8,10],
+                 'i_cifra_column':[3,9,3,3,11,3,9,3,9,3],
+                 'i_cifra_columnspan':[3,3,3,1,1,1,2,1,2,9]
+        }
+        for i in range (10):
+            super().__init__(i_cifra['dl_cifra'][i],i_cifra['i_cifra_row'][i],
+                             i_cifra['i_cifra_column'][i],i_cifra['i_cifra_columnspan'][i])
+        #буквы и цифры
+        i_b_c={'dl_b_c':[20,30,81,15],
+                 'i_b_c_row':[1,4,5,9],
+                 'i_b_c_column':[3,9,3,9],
+                 'i_b_c_columnspan':[3,3,9,2]
+        }
+        for i in range (4):
+            super().__init__(i_b_c['dl_b_c'][i],i_b_c['i_b_c_row'][i],
+                             i_b_c['i_b_c_column'][i],i_b_c['i_b_c_columnspan'][i])
+        #не проверить
+        ost={'dl':[20,12],'o_row':[6,9],'o_column':[7,3],'o_columnspan':[3,1]}
+        for i in range (2):
+            super().__init__(ost['dl'][i],ost['o_row'][i],
+                             ost['o_column'][i],ost['o_columnspan'][i])
 
 class pol:
     def __init__(self):
-        Label(root, text='пол                    ', font='Arial 10').grid(row=2,column= 0,columnspan= 3)
         var=IntVar()
         var.set(1)
         self.rad0 = Radiobutton(root,text="Мужской",variable=var,value=0).grid(row=2,column= 3,columnspan= 2)
@@ -113,18 +80,12 @@ class But_save:
 
 
 class zapusk_formy:
-    familiya(),imya(),otchestvo()
-    data_rogdeniya(),mesto_rogdeniya()
+    labels()
+    inputs()
     pol()
-    pass_ser(),pass_nom()
-    data_vydachy(),kem_vydan()
-    adres_reg()
-    N_kv(),kadastr_N(),etag()
-    o_S(),o_S_balk()
-    S_kuch(),S_gil()
-    N_zap_reg(),data_reg()
-    Sum_dog()
     But_save()
+    Label(root, text='кв.м.', font='Arial 10').grid(row=7,column= 11,columnspan= 1)
+    Label(root, text='кв.м.', font='Arial 10').grid(row=8,column= 11,columnspan= 1)
 
 
 zapusk_formy()
