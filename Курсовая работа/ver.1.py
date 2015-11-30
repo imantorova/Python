f53fc2d5d6dd8e453ba3ca2631df47668e8f0564
imantorova <a9711529@yandex.ru>
2015-12-01 00:53:23 +0300

    New (курсовая, дубль 2)

--- Курсовая работа/ver.1.py
+++ Курсовая работа/ver.1.py
@@ -1,114 +1,107 @@
 # coding: utf-8
 
-from tkinter import *
+import sys
+from PyQt5 import QtCore
+from PyQt5 import QtWidgets
+       
+class Ui_MainWindow(QtWidgets.QWidget):
 
-def vyvod_v_doc(event):
-    print('1111')
-    inp=inputs()
-    inp.save_data()
+    def setupUi(self, MainWindow):
+        
+        MainWindow.resize(634, 379)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.Widget = QtWidgets.QWidget(self.centralwidget)
+        self.Widget.setGeometry(QtCore.QRect(0, 10, 631, 341))
 
+    def viewUi(self, MainWindow):    
+        
+        self.l_lst={
+            'text':["Фамилия","имя","отчество","дата рождения", "место рождения", "пол","количество комнат","паспорт: серия",
+                    "номер","когда выдан","кем выдан","этаж","№ квартиры","адрес регистрации","площадь квартиры","площадь кухни",
+                    "площадь квартиры с балконом","жилая площадь","Кадастровый номер","Запись регистрации","дата регистрации",
+                    "Сумма договора","руб."],
+            'x':[2,210,400,2,179,456,337,2,220,462,2,190,2,2,208,500,2,365,2,2,441,2,440],
+            'y':[-1,0,0,29,30,30,150,60,60,60,90,150,153,120,181,181,181,181,210,240,240,270,270],
+            'width':[44,18,47,79,83,18,98,77,30,66,54,25,66,97,102,82,158,81,102,102,92,82,31],
+            'height':[16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16]
+            }
+        for i in range (23):
+                    
+            self.label = QtWidgets.QLabel(self.Widget)
+            self.label.setGeometry(QtCore.QRect(self.l_lst['x'][i], self.l_lst['y'][i], self.l_lst['width'][i], self.l_lst['height'][i]))
+            self.label.setText(self.l_lst['text'][i])
 
+        self.e_lst={
+            'x':[53,240,459,270,85,270,84,105,74,221,163,315,455,584,110,110,110],
+            'y':[0,0,0,30,60,60,90,120,150,150,180,180,180,180,210,240,270],
+            'width':[141,141,161,171,111,171,541,521,110,110,41,41,41,41,321,321,321],
+            'height':[20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20]
 
+            }    
 
-root=Tk()
+        for i in range (17):
+            self.lineEdit = QtWidgets.QLineEdit(self.Widget)
+            self.lineEdit.setGeometry(QtCore.QRect(self.e_lst['x'][i], self.e_lst['y'][i], self.e_lst['width'][i], self.e_lst['height'][i]))
+          
+        self.pushButton = QtWidgets.QPushButton(self.Widget)
+        self.pushButton.setGeometry(QtCore.QRect(550, 310, 75, 23))
+        self.pushButton.setText("Записать")
+       # self.pushButton.clicked.connect (self.funcSave())
 
+        self.dateEdit = QtWidgets.QDateEdit(self.Widget)
+        self.dateEdit.setGeometry(QtCore.QRect(86, 30, 85, 20))
+        self.dateEdit.setDateTime(QtCore.QDateTime(QtCore.QDate(1900, 1, 1)))
 
-class Shablon_text_label:
-    def __init__(self,text,r1,c1,cs1):
-        Label(root, text=text, font='Arial 10').grid(row=r1,column= c1,columnspan= cs1)
+        self.radioButton = QtWidgets.QRadioButton(self.Widget)
+        self.radioButton.setGeometry(QtCore.QRect(480, 30, 66, 18))
+        self.radioButton.setText("мужской")
 
-class Shablon_text_input:
-    def __init__(self,w,r2,c2,cs2):
-        self.e=Entry(root,width=w,bd=2)
-        self.e.grid(row=r2,column=c2,columnspan=cs2)
+        self.buttonGroup = QtWidgets.QButtonGroup(MainWindow)
 
-    def save_data(self,w,r2,c2,cs2):
-        self.e=Entry(root,width=w,bd=2)
-        self.e.grid(row=r2,column=c2,columnspan=cs2)
-        self.znach=self.e.get()
-        print ('znach v roditele',self.znach,'///')
+        self.buttonGroup.addButton(self.radioButton)
+        self.radioButton_2 = QtWidgets.QRadioButton(self.Widget)
+        self.radioButton_2.setGeometry(QtCore.QRect(552, 30, 66, 18))
+        self.radioButton_2.setText("женский")
 
-class labels(Shablon_text_label):
-    def __init__(self):
-        l={'name':['Фамилия','Имя','Отчество','дата рождения   ','место рождения','паспорт серия    ','номер','дата выдачи       ',
-                  'кем выдан ','адрес прописки   ','№ квартиры        ','Кадастровый №','Этаж     ','Общая площадь ',
-                  '  кв.м.;  Общая площадь с балконом','Площадь кухни  ','  кв.м.;                     Жилая площадь',
-                  'Рег. № записи    ','                             Дата регистрации','Сумма договора ','пол                    '],
-            'lab_row':[0,0,0,1,1,3,3,4,4,5,6,6,6,7,7,8,8,9,9,10,2],
-            'lab_column':[0,4,8,0,6,0,6,0,6,0,0,5,10,0,4,0,4,0,4,0,0],
-            'lab_columnspan':[2,2,2,3,3,3,3,3,3,3,3,2,1,3,5,3,5,3,5,3,3]
-        }
-        for i in range (21):
-            super().__init__(l['name'][i],l['lab_row'][i],l['lab_column'][i],l['lab_columnspan'][i])
+        self.buttonGroup.addButton(self.radioButton_2)
+        
 
-class inputs(Shablon_text_input):
-    def __init__(self):
-        #буквы!
-        i_bukva={'dl_bukva':[20,18,30,30],
-                 'i_bukva_row':[0,0,0,1],
-                 'i_bukva_column':[2,6,10,9],
-                 'i_bukva_columnspan':[2,2,2,3]
-        }
-        for i in range (4):
-            self.e=super().__init__(i_bukva['dl_bukva'][i],i_bukva['i_bukva_row'][i],
-                      i_bukva['i_bukva_column'][i],i_bukva['i_bukva_columnspan'][i])
+        self.dateEdit_2 = QtWidgets.QDateEdit(self.Widget)
+        self.dateEdit_2.setGeometry(QtCore.QRect(534, 60, 85, 20))
 
-        #цифры!
-        i_cifra={'dl_cifra':[20,30,20,12,13,12,15,12,15,81],
-                 'i_cifra_row':[3,3,4,6,6,7,7,8,8,10],
-                 'i_cifra_column':[3,9,3,3,11,3,9,3,9,3],
-                 'i_cifra_columnspan':[3,3,3,1,1,1,2,1,2,9]
-        }
+        self.radioButton_3 = QtWidgets.QRadioButton(self.Widget)
+        self.radioButton_3.setGeometry(QtCore.QRect(441, 151, 29, 18))
+        self.radioButton_3.setText("1")
 
-        for i in range (10):
-            self.cif=super().__init__(i_cifra['dl_cifra'][i],i_cifra['i_cifra_row'][i],
-                             i_cifra['i_cifra_column'][i],i_cifra['i_cifra_columnspan'][i])
+        self.buttonGroup_2 = QtWidgets.QButtonGroup(MainWindow)
 
-        #буквы и цифры
-        i_b_c={'dl_b_c':[20,30,81,15],
-                 'i_b_c_row':[1,4,5,9],
-                 'i_b_c_column':[3,9,3,9],
-                 'i_b_c_columnspan':[3,3,9,2]
-        }
+        self.buttonGroup_2.addButton(self.radioButton_3)
+        self.radioButton_4 = QtWidgets.QRadioButton(self.Widget)
+        self.radioButton_4.setGeometry(QtCore.QRect(476, 151, 29, 18))
+        self.radioButton_4.setText("2")
 
-        for i in range (4):
-            self.b_c=super().__init__(i_b_c['dl_b_c'][i],i_b_c['i_b_c_row'][i],
-                             i_b_c['i_b_c_column'][i],i_b_c['i_b_c_columnspan'][i])
+        self.buttonGroup_2.addButton(self.radioButton_4)
+        self.radioButton_5 = QtWidgets.QRadioButton(self.Widget)
+        self.radioButton_5.setGeometry(QtCore.QRect(511, 151, 29, 18))
+        self.radioButton_5.setText("3")
 
-        #не проверить
-        ost={'dl':[20,12],'o_row':[6,9],'o_column':[7,3],'o_columnspan':[3,1]}
-        for i in range (2):
-            self.ostaln=super().__init__(ost['dl'][i],ost['o_row'][i],
-                                         ost['o_column'][i],ost['o_columnspan'][i])
-    def save_data(self):
-        self.znach_b=super().save_data(20,0,2,2)#первое поле с фамилией
-        print('znach v klasse',self.znach_b,'///')
+        self.buttonGroup_2.addButton(self.radioButton_5)
 
-class pol:
-    def __init__(self):
-        var=IntVar()
-        var.set(1)
-        self.rad0 = Radiobutton(root,text="Мужской",variable=var,value=0).grid(row=2,column= 3,columnspan= 2)
-        self.rad1 = Radiobutton(root,text="Женский",variable=var,value=1).grid(row=2,column= 5,columnspan= 2)
-
-class But_save:
-    def __init__(self):
-        self.but=Button(root, text = 'СОХРАНИТЬ',font='Arial 10')
-        self.but.grid(row=11,column=11,columnspan= 2)
-        self.but.bind("<Button-1>",vyvod_v_doc)
+        self.dateEdit_3 = QtWidgets.QDateEdit(self.Widget)
+        self.dateEdit_3.setGeometry(QtCore.QRect(539, 240, 85, 20))
+        self.dateEdit_3.setDateTime(QtCore.QDateTime(QtCore.QDate(2015, 1, 1)))
 
 
+        MainWindow.setCentralWidget(self.centralwidget)
+        MainWindow.show()      
+        
 
-class zapusk_formy:
-    labels()
-    inp = inputs()
-    pol()
-    But_save()
-    Label(root, text='кв.м.', font='Arial 10').grid(row=7,column= 11,columnspan= 1)
-    Label(root, text='кв.м.', font='Arial 10').grid(row=8,column= 11,columnspan= 1)
-   # inp.save_data()
-
-zapusk_formy()
-
-root.mainloop()
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    ui.viewUi (MainWindow)
+    sys.exit(app.exec_())
 
